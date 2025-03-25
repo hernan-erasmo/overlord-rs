b89d39b24db5d1a2bd19ce2265f268b64df9ccec
Hernan Erasmo <dherasmo@gmail.com>
2025-03-25 17:22:55 -0300

    refactor: adjusted bur to use new generate_reserve_details_by_asset

--- crates/profito-rs/bin/bpchecker.rs
+++ crates/profito-rs/bin/bpchecker.rs
@@ -1,11 +1,10 @@
 use alloy::{
-    primitives::{address, aliases::U24, utils::format_units, Address, U256},
+    primitives::{aliases::U24, utils::format_units, Address, U256},
     providers::{IpcConnect, Provider, ProviderBuilder, RootProvider},
     pubsub::PubSubFrontend,
-    sol_types::sol,
 };
 use profito_rs::{
-    calculations::{get_best_fee_tier_for_swap, percent_div, percent_mul},
+    calculations::{percent_div, percent_mul},
     constants::{
         AAVE_ORACLE_ADDRESS, AAVE_V3_POOL_ADDRESS, AAVE_V3_PROTOCOL_DATA_PROVIDER_ADDRESS,
         AAVE_V3_PROVIDER_ADDRESS, AAVE_V3_UI_POOL_DATA_PROVIDER_ADDRESS, UNISWAP_V3_FACTORY, WETH,
@@ -16,9 +15,9 @@ use profito_rs::{
         IUiPoolDataProviderV3::{AggregatedReserveData, UserReserveData},
         UniswapV3Factory, UniswapV3Pool, ERC20,
     },
-    utils::{ReserveConfigurationData, ReserveConfigurationEnhancedData},
+    utils::{ReserveConfigurationEnhancedData, generate_reserve_details_by_asset},
 };
-use std::{collections::HashMap, env};
+use std::{collections::HashMap, env, sync::Arc};
 
 #[derive(Debug)]
 struct BestPair {
@@ -30,218 +29,10 @@ struct BestPair {
     liquidation_protocol_fee_amount: U256,
 }
 
-async fn generate_reserve_details_by_asset(
-    provider: RootProvider<PubSubFrontend>,
-    reserves: Vec<UserReserveData>,
-) -> ReserveConfigurationData {
-    let mut symbols_by_address = HashMap::new();
-    symbols_by_address.insert(
-        address!("c02aaa39b223fe8d0a0e5c4f27ead9083c756cc2"),
-        "WETH".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("7f39c581f595b53c5cb19bd0b3f8da6c935e2ca0"),
-        "wstETH".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("2260fac5e5542a773aa44fbcfedf7c193bc2c599"),
-        "WBTC".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("a0b86991c6218b36c1d19d4a2e9eb0ce3606eb48"),
-        "USDC".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("6b175474e89094c44da98b954eedeac495271d0f"),
-        "DAI".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("514910771af9ca656af840dff83e8264ecf986ca"),
-        "LINK".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("7fc66500c84a76ad7e9c93437bfc5ac33e2ddae9"),
-        "AAVE".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("be9895146f7af43049ca1c1ae358b0541ea49704"),
-        "cbETH".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("dac17f958d2ee523a2206206994597c13d831ec7"),
-        "USDT".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("ae78736cd615f374d3085123a210448e74fc6393"),
-        "rETH".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("5f98805a4e8be255a32880fdec7f6728c6568ba0"),
-        "LUSD".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("d533a949740bb3306d119cc777fa900ba034cd52"),
-        "CRV".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("9f8f72aa9304c8b593d555f12ef6589cc3a579a2"),
-        "MKR".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("c011a73ee8576fb46f5e1c5751ca3b9fe0af2a6f"),
-        "SNX".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("ba100000625a3754423978a60c9317c58a424e3d"),
-        "BAL".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("1f9840a85d5af5bf1d1762f925bdaddc4201f984"),
-        "UNI".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("5a98fcbea516cf06857215779fd812ca3bef1b32"),
-        "LDO".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("c18360217d8f7ab5e7c516566761ea12ce7f9d72"),
-        "ENS".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("111111111117dc0aa78b770fa6a738034120c302"),
-        "1INCH".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("853d955acef822db058eb8505911ed77f175b99e"),
-        "FRAX".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("40d16fc0246ad3160ccc09b8d0d3a2cd28ae6c2f"),
-        "GHO".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("d33526068d116ce69f19a9ee46f0bd304f21a51f"),
-        "RPL".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("83f20f44975d03b1b09e64809b757c47f942beea"),
-        "sDAI".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("af5191b0de278c7286d6c7cc6ab6bb8a73ba2cd6"),
-        "STG".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("defa4e8a7bcba345f687a2f1456f5edd9ce97202"),
-        "KNC".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("3432b6a60d23ca0dfca7761b7ab56459d9c964d0"),
-        "FXS".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("f939e0a03fb07f59a73314e73794be0e57ac1b4e"),
-        "crvUSD".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("6c3ea9036406852006290770bedfcaba0e23a0e8"),
-        "PYUSD".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("cd5fe23c85820f7b72d0926fc9b05b43e359b7ee"),
-        "weETH".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("f1c9acdc66974dfb6decb12aa385b9cd01190e38"),
-        "osETH".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("4c9edd5852cd905f086c759e8383e09bff1e68b3"),
-        "USDe".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("a35b1b31ce002fbf2058d22f30f95d405200a15b"),
-        "ETHx".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("9d39a5de30e57443bff2a8307a4256c8797a3497"),
-        "sUSDe".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("18084fba666a33d37592fa2633fd49a74dd93a88"),
-        "tBTC".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("cbb7c0000ab88b473b1f5afd9ef808440eed33bf"),
-        "cbBTC".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("dc035d45d973e3ec169d2276ddab16f1e407384f"),
-        "USDS".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("a1290d69c65a6fe4df752f95823fae25cb99e5a7"),
-        "rsETH".to_string(),
-    );
-    symbols_by_address.insert(
-        address!("8236a87084f8b84306f72007f36f2618a5634494"),
-        "LBTC".to_string(),
-    );
-
-    let mut configuration_data: ReserveConfigurationData = HashMap::new();
-    let aave_config =
-        AaveProtocolDataProvider::new(AAVE_V3_PROTOCOL_DATA_PROVIDER_ADDRESS, provider.clone());
-    let unknown_asset = String::from("unknown_asset");
-    for reserve in reserves {
-        let symbol = symbols_by_address
-            .get(&reserve.underlyingAsset)
-            .unwrap_or(&unknown_asset);
-        let data = match aave_config
-            .getReserveConfigurationData(reserve.underlyingAsset)
-            .call()
-            .await
-        {
-            Ok(reserve_config) => Some(reserve_config),
-            Err(e) => {
-                eprintln!(
-                    "Failed to get reserve configuration data for asset {}: {}",
-                    reserve.underlyingAsset, e
-                );
-                None
-            }
-        };
-        let liquidation_fee = match aave_config
-            .getLiquidationProtocolFee(reserve.underlyingAsset)
-            .call()
-            .await
-        {
-            Ok(fee_response) => Some(fee_response._0),
-            Err(e) => {
-                eprintln!(
-                    "Failed to get reserve liquidation fee for asset {}: {}",
-                    reserve.underlyingAsset, e
-                );
-                None
-            }
-        };
-        if let (Some(data), Some(liquidation_fee)) = (data, liquidation_fee) {
-            configuration_data.insert(
-                reserve.underlyingAsset,
-                ReserveConfigurationEnhancedData {
-                    symbol: symbol.clone(),
-                    data,
-                    liquidation_fee,
-                },
-            );
-        }
-    }
-    configuration_data
-}
-
 /// Get's the list of user reserves, but only returns those that the user has at least some debt or collateral and,
 /// for the later, the ones that are allowed to be used as collateral
 async fn get_user_reserves_data(
-    provider: RootProvider<PubSubFrontend>,
+    provider: Arc<RootProvider<PubSubFrontend>>,
     user_address: Address,
 ) -> Vec<UserReserveData> {
     let ui_data =
@@ -268,7 +59,7 @@ async fn get_user_reserves_data(
         .collect()
 }
 
-async fn get_user_health_factor(provider: RootProvider<PubSubFrontend>, user: Address) -> U256 {
+async fn get_user_health_factor(provider: Arc<RootProvider<PubSubFrontend>>, user: Address) -> U256 {
     let pool = AaveV3Pool::new(AAVE_V3_POOL_ADDRESS, provider.clone());
     match pool.getUserAccountData(user).call().await {
         Ok(account_data) => account_data.healthFactor,
@@ -282,7 +73,7 @@ async fn get_user_health_factor(provider: RootProvider<PubSubFrontend>, user: Ad
 /// This function is the rust equivalent of _calculateDebt() defined in LiquidationLogic.sol
 /// https://github.com/aave/aave-v3-core/blob/b74526a7bc67a3a117a1963fc871b3eb8cea8435/contracts/protocol/libraries/logic/LiquidationLogic.sol#L363
 async fn calculate_debt(
-    provider: RootProvider<PubSubFrontend>,
+    provider: Arc<RootProvider<PubSubFrontend>>,
     user: Address,
     debt_asset: Address,
     user_health_factor: U256,
@@ -619,7 +410,7 @@ fn wad_div(a: U256, b: U256) -> U256 {
 
 /// https://github.com/aave-dao/aave-v3-origin/blob/a0512f8354e97844a3ed819cf4a9a663115b8e20/src/contracts/protocol/libraries/logic/GenericLogic.sol#L249
 async fn get_user_balance_in_base_currency(
-    provider: RootProvider<PubSubFrontend>,
+    provider: Arc<RootProvider<PubSubFrontend>>,
     reserve: Address,
     a_token_address: Address,
     user_address: Address,
@@ -654,7 +445,7 @@ async fn get_user_balance_in_base_currency(
 
 /// https://github.com/aave-dao/aave-v3-origin/blob/a0512f8354e97844a3ed819cf4a9a663115b8e20/src/contracts/protocol/libraries/logic/GenericLogic.sol#L219
 async fn get_user_debt_in_base_currency(
-    provider: RootProvider<PubSubFrontend>,
+    provider: Arc<RootProvider<PubSubFrontend>>,
     reserve: Address,
     variable_debt_token_address: Address,
     user_address: Address,
@@ -696,7 +487,7 @@ async fn get_user_debt_in_base_currency(
 /// https://github.com/aave-dao/aave-v3-origin/blob/bb6ea42947f349fe8182a0ea30c5a7883d1f9ed1/src/contracts/protocol/libraries/logic/GenericLogic.sol#L63
 /// except for emode support. We don't do that here.
 async fn calculate_user_account_data(
-    provider: RootProvider<PubSubFrontend>,
+    provider: Arc<RootProvider<PubSubFrontend>>,
     user_address: Address,
     reserves_list: Vec<Address>,
     reserves_data: Vec<AggregatedReserveData>,
@@ -856,7 +647,7 @@ fn print_debt_collateral_title(
     );
 }
 
-async fn get_reserves_list(provider: RootProvider<PubSubFrontend>) -> Vec<Address> {
+async fn get_reserves_list(provider: Arc<RootProvider<PubSubFrontend>>) -> Vec<Address> {
     /*
        According to https://github.com/aave-dao/aave-v3-origin/blob/a0512f8354e97844a3ed819cf4a9a663115b8e20/src/contracts/protocol/pool/Pool.sol#L532
        the reserves list is ordered the same way as the _reserveList storage in the Pool contract.
@@ -874,7 +665,7 @@ async fn get_reserves_list(provider: RootProvider<PubSubFrontend>) -> Vec<Addres
     }
 }
 
-async fn get_reserves_data(provider: RootProvider<PubSubFrontend>) -> Vec<AggregatedReserveData> {
+async fn get_reserves_data(provider: Arc<RootProvider<PubSubFrontend>>) -> Vec<AggregatedReserveData> {
     /*
        According to https://github.com/aave-dao/aave-v3-origin/blob/a0512f8354e97844a3ed819cf4a9a663115b8e20/src/contracts/helpers/UiPoolDataProviderV3.sol#L45
        the reserves data is ordered the same way as the reserves list (it actually calls pool.getReservesList() and uses it as index)
@@ -892,9 +683,8 @@ async fn get_reserves_data(provider: RootProvider<PubSubFrontend>) -> Vec<Aggreg
     }
 }
 
-async fn get_asset_price(provider: RootProvider<PubSubFrontend>, asset: Address) -> U256 {
-    let aave_oracle: AaveOracle::AaveOracleInstance<PubSubFrontend, RootProvider<PubSubFrontend>> =
-        AaveOracle::new(AAVE_ORACLE_ADDRESS, provider.clone());
+async fn get_asset_price(provider: Arc<RootProvider<PubSubFrontend>>, asset: Address) -> U256 {
+    let aave_oracle = AaveOracle::new(AAVE_ORACLE_ADDRESS, provider.clone());
     match aave_oracle.getAssetPrice(asset).call().await {
         Ok(price_response) => price_response._0,
         Err(e) => {
@@ -949,7 +739,7 @@ fn calculate_actual_debt_to_liquidate(
 
 /// https://github.com/aave-dao/aave-v3-origin/blob/e8f6699e58038cbe3aba982557ceb2b0dda303a0/src/contracts/protocol/libraries/logic/LiquidationLogic.sol#L633
 async fn calculate_available_collateral_to_liquidate(
-    provider: RootProvider<PubSubFrontend>,
+    provider: Arc<RootProvider<PubSubFrontend>>,
     collateral_asset: Address,
     collateral_decimals: U256,
     // all original args for this function under this line
@@ -1104,7 +894,7 @@ async fn get_uniswap_v3_pools(
 /// UniswapV3 fees are hundredths of basis points: 1% == 10000; 0,3% == 3000; 0,05% == 500; 0,01% == 100
 /// Calculate and return the lowest fee tier for which there's enough liquidity
 async fn calculate_best_swap_fees(
-    provider: RootProvider<PubSubFrontend>,
+    provider: Arc<RootProvider<PubSubFrontend>>,
     collateral_asset: Address,
     debt_asset: Address,
 ) -> (U24, U24) {
@@ -1160,7 +950,8 @@ async fn main() {
 
     // Setup provider
     let ipc = IpcConnect::new(ipc_path.to_string());
-    let provider = ProviderBuilder::new().on_ipc(ipc).await.unwrap();
+    let provider = ProviderBuilder::new().on_ipc(ipc).await?;
+    let provider = Arc::new(provider);
 
     let block_number = provider.get_block_number().await.unwrap_or_default();
     println!(
@@ -1169,11 +960,12 @@ async fn main() {
     );
 
     // Get user reserves data
+    // TODO(Hernan): can we remove this as part of the streamlining of generate_reserve_details_by_asset?
     let user_reserves_data = get_user_reserves_data(provider.clone(), user_address).await;
 
     // Create reserve configuration struct
     let reserves_configuration =
-        generate_reserve_details_by_asset(provider.clone(), user_reserves_data.clone()).await;
+        generate_reserve_details_by_asset(provider.clone()).await;
     let assets_borrowed = user_reserves_data
         .iter()
         .filter(|reserve| reserve.scaledVariableDebt > U256::ZERO)
