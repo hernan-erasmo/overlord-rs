f18bbd3d906e0cc7e63552482ad78b442cd8465b
Hernan Erasmo <dherasmo@gmail.com>
2025-03-26 16:43:12 -0300

    refactor: the way get_price works

--- crates/profito-rs/src/cache/price.rs
+++ crates/profito-rs/src/cache/price.rs
@@ -8,7 +8,7 @@ use std::{
     collections::{HashMap, HashSet, VecDeque},
     sync::Arc,
 };
-use tracing::{info, warn, error};
+use tracing::{info, error};
 
 #[derive(Debug, Clone)]
 pub struct PriceCache {
@@ -42,10 +42,9 @@ impl PriceCache {
         }
 
         // Now that override_price is called _before_ processing candidates, it should never
-        // happen that we already have a trace present in the cache.
-        // I don't want to kill the program in this case, so I'm returning true as if nothing
-        // happened. But I'm logging an error nonetheless because this would be a most curious
-        // occurrence.
+        // happen that we already have a trace present in the cache.  I don't want to kill the program
+        // in this case, so I'm returning true as if nothing happened. But I'm logging an error nonetheless
+        // because this would be a most curious occurrence.
         if self.overriden_traces.contains(&trace_id) {
             error!("trace_id {} was present in the price cache", trace_id);
             return true;
@@ -63,9 +62,17 @@ impl PriceCache {
         }
 
         // Mark the trace as overriden
-        // This hashset get's it's element removed when the trace is dropped from the cache
-        // at price_cache.get_price()
         self.overriden_traces.insert(trace_id.clone());
+        self.trace_order.push_back(trace_id.clone());
+
+        // If we have reached the maximum number of traces, drop the oldest one
+        if self.trace_order.len() >= self.max_traces {
+            if let Some(oldest_trace) = self.trace_order.pop_front() {
+                info!("Dropping prices cached for {}", oldest_trace);
+                self.prices.remove(&oldest_trace);
+                self.overriden_traces.remove(&oldest_trace);
+            }
+        }
 
         true
     }
@@ -76,37 +83,26 @@ impl PriceCache {
         trace_id: String,
         oracle: AaveOracle::AaveOracleInstance<PubSubFrontend, Arc<RootProvider<PubSubFrontend>>>,
     ) -> Result<U256, Box<dyn std::error::Error + Send + Sync>> {
-        // Check if price exists for this trace_id
+        // Check if there are overrides for this price
         if let Some(prices) = self.prices.get(&trace_id) {
+            // If there are overrides, and the price is present, return it
             if let Some(&price) = prices.get(&reserve) {
                 return Ok(price);
             }
-        }
 
-        // Fetch new price
-        let price = match oracle.getAssetPrice(reserve).call().await {
-            Ok(price_response) => price_response._0,
-            Err(e) => return Err(format!("Couldn't fetch price for {}: {}", reserve, e).into()),
-        };
+            // If there are overrides, but the price is not present, fetch it
+            let price = match oracle.getAssetPrice(reserve).call().await {
+                Ok(price_response) => price_response._0,
+                Err(e) => return Err(format!("Couldn't fetch price for {}: {}", reserve, e).into()),
+            };
 
-        // Add new trace_id if not exists
-        if !self.prices.contains_key(&trace_id) {
-            if self.trace_order.len() >= self.max_traces {
-                if let Some(oldest_trace) = self.trace_order.pop_front() {
-                    info!("Dropping prices cached for {}", oldest_trace);
-                    self.prices.remove(&oldest_trace);
-                    self.overriden_traces.remove(&oldest_trace);
-                }
+            // And update it for future calls within this trace_id context
+            if let Some(prices) = self.prices.get_mut(&trace_id) {
+                prices.insert(reserve, price);
             }
-            self.trace_order.push_back(trace_id.clone());
-            self.prices.insert(trace_id.clone(), HashMap::new());
-        }
 
-        // Update price
-        if let Some(prices) = self.prices.get_mut(&trace_id) {
-            prices.insert(reserve, price);
+            return Ok(price);
         }
-
-        Ok(price)
+        Err(format!("price for {} within trace_id {} not found in the price cache", reserve, trace_id).into())
     }
 }
