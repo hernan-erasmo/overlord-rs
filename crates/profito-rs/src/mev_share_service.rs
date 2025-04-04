dfb4938c44995aae7a02cb97e2b44978861b7763
Hernan Erasmo <dherasmo@gmail.com>
2025-04-04 19:37:59 -0300

    feat: add submit_simple_liquidation_bundle stub

--- crates/profito-rs/src/mev_share_service.rs
+++ crates/profito-rs/src/mev_share_service.rs
@@ -1,14 +1,14 @@
 use ethers_core::{
-    rand::thread_rng,
-    k256::ecdsa::SigningKey
+    k256::ecdsa::SigningKey, rand::thread_rng, types::{transaction::eip2718::TypedTransaction, H256}
 };
-use ethers_signers::{LocalWallet, Wallet};
+use ethers_signers::{LocalWallet, Signer, Wallet};
 use jsonrpsee::http_client::{transport::{Error as HttpError, HttpBackend}, HttpClient, HttpClientBuilder};
-use mev_share::rpc::{FlashbotsSigner, FlashbotsSignerLayer};
+use mev_share::rpc::{BundleItem, FlashbotsSigner, FlashbotsSignerLayer, MevApiClient, SendBundleRequest};
 use once_cell::sync::OnceCell;
 use std::sync::Arc;
 use tokio::sync::Mutex;
 use tower::{util::MapErr, ServiceBuilder};
+use tracing::info;
 
 type MevShareClient = HttpClient<MapErr<FlashbotsSigner<Wallet<SigningKey>, HttpBackend>, fn(Box<dyn std::error::Error + Send + Sync>) -> HttpError>>;
 
@@ -17,6 +17,8 @@ static MEVSHARE_CLIENT: OnceCell<Arc<MevShareClient>> = OnceCell::new();
 #[derive(Clone)]
 pub struct MevShareService {
     initialization: Arc<Mutex<()>>,
+    fb_signer: LocalWallet,
+    tx_signer: LocalWallet,
 }
 
 impl Default for MevShareService {
@@ -28,6 +30,8 @@ impl Default for MevShareService {
 impl MevShareService {
     pub fn new() -> Self {
         Self {
+            fb_signer: LocalWallet::new(&mut thread_rng()),
+            tx_signer: LocalWallet::new(&mut thread_rng()),
             initialization: Arc::new(Mutex::new(())),
         }
     }
@@ -44,14 +48,8 @@ impl MevShareService {
             return Ok(client.clone());
         }
 
-        // The signer used to authenticate bundles
-        let fb_signer = LocalWallet::new(&mut thread_rng());
-
-        // The signer used to sign our transactions
-        let tx_signer = LocalWallet::new(&mut thread_rng());
-
         // Set up flashbots-style auth middleware
-        let signing_middleware = FlashbotsSignerLayer::new(fb_signer);
+        let signing_middleware = FlashbotsSignerLayer::new(self.fb_signer.clone());
         let service_builder = ServiceBuilder::new()
             // map signer errors to http errors
             .map_err(HttpError::Http as fn(Box<dyn std::error::Error + Send + Sync>) -> HttpError)
@@ -68,5 +66,36 @@ impl MevShareService {
             .set(client.clone().into())
             .map_err(|_| "Failed to SET Mev-Share client on cache")?;
         Ok(client.into())
+    }
+
+    pub async fn submit_simple_liquidation_bundle(
+        &self,
+        pub_tx: H256,
+        foxdie_tx: TypedTransaction,
+    ) -> Result<(), Box<dyn std::error::Error>> {
+        let signature = self.tx_signer.sign_transaction(&foxdie_tx.clone().into()).await?;
+        let bytes = foxdie_tx.rlp_signed(&signature);
+        let bundle_body = vec![
+            BundleItem::Hash { hash: pub_tx },
+            BundleItem::Tx { tx: bytes, can_revert: false },
+        ];
+        let bundle = SendBundleRequest {
+            bundle_body, ..Default::default()
+        };
+
+        /*
+            Uncomment when ready
+
+            // Send bundle
+            let client = &*self.get_client().await?;
+            let send_res = MevApiClient::send_bundle(client, bundle.clone()).await;
+            info!("Got a bundle response: {:?}", send_res);
+
+            // Simulate bundle
+            let sim_res = MevApiClient::sim_bundle(client, bundle.clone(), Default::default()).await;
+            info!("Got a simulation response: {:?}", sim_res);
+         */
+
+        Ok(())
     }
 }
